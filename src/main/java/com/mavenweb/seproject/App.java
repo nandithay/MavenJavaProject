a65196fab617c948d9bad5ca73e9e8ced5f99b9a
nandithay <123803495+nandithay@users.noreply.github.com>
2025-01-16 14:12:57 +0530

    Add files via upload

---
+++ src/main/java/com/mavenweb/seproject/App.java
@@ -0,0 +1,13 @@
+package com.mavenweb.seproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Name:Y.Nanditha RollNo: 22BD1A66K1" );
+    }
+}
